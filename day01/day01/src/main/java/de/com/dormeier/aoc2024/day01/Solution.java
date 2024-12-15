e74da527395bcc8b6631b2960ebcc2b302efb27e
Philipp <30900810+DerReparator@users.noreply.github.com>
2024-12-15 11:43:35 +0100

    solved Day01_1

--- day01/day01/src/main/java/de/com/dormeier/aoc2024/day01/Solution.java
+++ day01/day01/src/main/java/de/com/dormeier/aoc2024/day01/Solution.java
@@ -3,6 +3,8 @@ package de.com.dormeier.aoc2024.day01;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Advent of Code 2024 - Day 01
@@ -11,13 +13,13 @@ public class Solution
 {
 	public static void main(String[] args) throws IOException
     {
-		if (args.length != 3) {
-			System.out.println("Ivalid args. Expected exactly 2 args: pathToInputDay1 pathToInputDay2");
+		if (args.length != 2) {
+			System.out.println("Invalid args. Expected exactly 2 args: pathToInputDay1 pathToInputDay2");
 			return;
 		}
 
-		String inputPart1 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[1])));
-		String inputPart2 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[2])));
+		String inputPart1 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[0])));
+		String inputPart2 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[1])));
 
 		Object solutionPart1 = solvePart1(inputPart1);
 		Object solutionPart2 = solvePart2(inputPart2);
@@ -33,8 +35,26 @@ public class Solution
 	 * @return The solution to the first part.
 	 */
 	protected static Object solvePart1(String input) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> firstList = new ArrayList<Integer>();
+		List<Integer> secondList = new ArrayList<Integer>();
+		
+		for (String line : input.lines().toArray(String[]::new)) {
+			String[] splitLine = line.split("   ");
+			
+			firstList.add(Integer.parseInt(splitLine[0]));
+			secondList.add(Integer.parseInt(splitLine[1]));
+		}
+
+		firstList.sort((i1, i2) -> Integer.compare(i1, i2));
+		secondList.sort((i1, i2) -> Integer.compare(i1, i2));
+
+		int result = 0;
+
+		for (int i = 0; i < firstList.size(); ++i) {
+			result += Math.abs(firstList.get(i) - secondList.get(i));
+		}
+
+		return result;
 	}
 
 	/**
--- day01/day01/src/test/java/de/com/dormeier/aoc2024/day01/TestPart1.java
+++ day01/day01/src/test/java/de/com/dormeier/aoc2024/day01/TestPart1.java
@@ -2,28 +2,26 @@ package de.com.dormeier.aoc2024.day01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
+import io.hosuaby.inject.resources.junit.jupiter.GivenTextResource;
+import io.hosuaby.inject.resources.junit.jupiter.TestWithResources;
+
+@TestWithResources
 public class TestPart1 {
+
+	@GivenTextResource("day01_1_test1.input")
+	String inputPart1_1;
+	
+	@GivenTextResource("day01_1_test1.output")
+	String expectedPart1_1;
 
 	@Test
 	public void TestPart1_1() throws IOException {
+		Object result = Solution.solvePart1(inputPart1_1);
 
-		try (InputStreamReader isr = new InputStreamReader(getClass().getResourceAsStream("day01_1_test1.input"));
-				BufferedReader reader = new BufferedReader(isr)) {
-			String input = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-			String expected = Files.readString(Paths.get("test/resources/day01_1_test1.output"));
-
-			Object result = Solution.solvePart1(input);
-
-			assertEquals(expected, result.toString());
-		}
+		assertEquals(expectedPart1_1, result.toString());
 	}
 }

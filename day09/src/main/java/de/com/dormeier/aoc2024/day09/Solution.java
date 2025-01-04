1f1da8a9a5013388c603c421b20f7e95f81fdf1a
Philipp <30900810+DerReparator@users.noreply.github.com>
2025-01-04 18:48:50 +0100

    added setup and additional unit test for Day 09, Part 1

---
+++ day09/src/main/java/de/com/dormeier/aoc2024/day09/Solution.java
@@ -0,0 +1,84 @@
+package de.com.dormeier.aoc2024.day09;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Advent of Code 2024 - Day 09
+ */
+public class Solution
+{
+	public static final int FREE_SPACE = -1;
+
+    public static void main(String[] args) throws IOException
+    {
+		if (args.length != 2) {
+			System.out.println("Invalid args. Expected exactly 2 args: pathToInputDay1 pathToInputDay2");
+			return;
+		}
+
+		String inputPart1 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[0])));
+		String inputPart2 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[1])));
+
+		Object solutionPart1 = solvePart1(inputPart1);
+		Object solutionPart2 = solvePart2(inputPart2);
+
+		System.out.println("Solution Part 1: " + solutionPart1);
+		System.out.println("Solution Part 2: " + solutionPart2);
+    }
+
+    /**
+	 * Solve the first part.
+	 * 
+	 * @param input The textual input
+	 * @return The solution to the first part.
+	 */
+	protected static Object solvePart1(String input) {
+		String diskmap = input;
+
+		List<Integer> compactedDiskmap = CompactDiskMap(diskmap);
+        return "";
+	}
+
+	public static List<Integer> CompactDiskMap(String diskmap) {
+		List<Integer> expandedDiskmap = new ArrayList<>();
+		int idNumber = 0;
+
+		for (int diskmapIndex = 0; diskmapIndex < diskmap.length(); ++diskmapIndex) {
+			if (diskmapIndex % 2 == 0) {
+				/* file length */
+				for (int j = 0; j < Integer.parseInt("" + diskmap.charAt(diskmapIndex)); ++j) {
+					expandedDiskmap.add(idNumber);
+				}
+				++idNumber;
+			} else {
+				/* free space */
+				for (int j = 0; j < Integer.parseInt("" + diskmap.charAt(diskmapIndex)); ++j) {
+					expandedDiskmap.add(FREE_SPACE);
+				}
+			}
+		}
+
+		System.out.println("Expanded to: " + expandedDiskmap);
+
+		while (expandedDiskmap.indexOf(FREE_SPACE) > -1) {
+			expandedDiskmap.set(expandedDiskmap.indexOf(FREE_SPACE), expandedDiskmap.removeLast());
+		}
+
+		return expandedDiskmap;
+	}
+
+	/**
+	 * Solve the second part.
+	 * 
+	 * @param input The textual input
+	 * @return The solution to the second part.
+	 */
+	protected static Object solvePart2(String input) {
+		/* TODO your solution for Part 2 goes here. */        
+        return "";
+	}
+}
---
+++ day09/src/test/java/de/com/dormeier/aoc2024/day09/TestPart1.java
@@ -0,0 +1,38 @@
+package de.com.dormeier.aoc2024.day09;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import io.hosuaby.inject.resources.junit.jupiter.GivenTextResource;
+import io.hosuaby.inject.resources.junit.jupiter.TestWithResources;
+
+/**
+ * Tests Day 09 - Part 1.
+ */
+@TestWithResources
+public class TestPart1 {
+
+	@GivenTextResource("day09_part_1_test1.input")
+	String inputPart1_1;
+	
+	@GivenTextResource("day09_part_1_test1.output")
+	String expectedPart1_1;
+
+	@Test
+	public void TestPart1_1() throws IOException {
+		Object result = Solution.solvePart1(inputPart1_1);
+
+		assertEquals(expectedPart1_1, result.toString());
+	}
+
+	@Test
+	public void TestCompactDiskmap() {
+		String expected = "0099811188827773336446555566"; /* taken from adventofcode.com */
+		var result = Solution.CompactDiskMap(inputPart1_1);
+
+		assertEquals(expected, String.join("", result.stream().map(i -> i.toString()).toList()));
+	}
+}

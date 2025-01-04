c037f241526d81d045770f239d6e4502485d3050
Phil <30900810+DerReparator@users.noreply.github.com>
2025-01-04 10:06:48 +0100

    added Day 08, Part 1 with 3 test cases

---
+++ day08/src/test/java/de/com/dormeier/aoc2024/TestPart1.java
@@ -0,0 +1,56 @@
+package de.com.dormeier.aoc2024;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import io.hosuaby.inject.resources.junit.jupiter.GivenTextResource;
+import io.hosuaby.inject.resources.junit.jupiter.TestWithResources;
+
+/**
+ * Tests Day 08 - Part 1.
+ */
+@TestWithResources
+public class TestPart1 {
+
+	@GivenTextResource("day08_part_1_test1.input")
+	String inputPart1_1;
+	
+	@GivenTextResource("day08_part_1_test1.output")
+	String expectedPart1_1;
+
+	@Test
+	public void TestPart1_1() throws IOException {
+		Object result = Solution.solvePart1(inputPart1_1);
+
+		assertEquals(expectedPart1_1, result.toString());
+	}
+
+	@GivenTextResource("day08_part_1_testEasy.input")
+	String inputPart1Easy;
+	
+	@GivenTextResource("day08_part_1_testEasy.output")
+	String expectedPart1Easy;
+
+	@Test
+	public void TestPart1Easy() throws IOException {
+		Object result = Solution.solvePart1(inputPart1Easy);
+
+		assertEquals(expectedPart1Easy, result.toString());
+	}
+
+	@GivenTextResource("day08_part_1_testOutOfMap.input")
+	String inputPart1OutOfMap;
+	
+	@GivenTextResource("day08_part_1_testOutOfMap.output")
+	String expectedPart1OutOfMap;
+
+	@Test
+	public void TestPart1OutOfMap() throws IOException {
+		Object result = Solution.solvePart1(inputPart1OutOfMap);
+
+		assertEquals(expectedPart1OutOfMap, result.toString());
+	}
+}

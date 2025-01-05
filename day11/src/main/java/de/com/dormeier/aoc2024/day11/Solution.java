28b47fef766af65338bac77424ca1f774dc7562f
Philipp <30900810+DerReparator@users.noreply.github.com>
2025-01-05 17:04:54 +0100

    finished multithreaded implementation that doesn't work in a good timeframe

--- day11/src/main/java/de/com/dormeier/aoc2024/day11/Solution.java
+++ day11/src/main/java/de/com/dormeier/aoc2024/day11/Solution.java
@@ -6,12 +6,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.RunnableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 /**
@@ -29,10 +28,10 @@ public class Solution
 		String inputPart1 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[0])));
 		String inputPart2 = String.join(System.lineSeparator(), Files.readAllLines(Paths.get(args[1])));
 
-		Object solutionPart1 = solvePart1(inputPart1, BLINKS_PART1);
+		// Object solutionPart1 = solvePart1(inputPart1, BLINKS_PART1);
 		Object solutionPart2 = solvePart2(inputPart2);
 
-		System.out.println("Solution Part 1: " + solutionPart1);
+//		System.out.println("Solution Part 1: " + solutionPart1);
 		System.out.println("Solution Part 2: " + solutionPart2);
     }
 
@@ -49,8 +48,6 @@ public class Solution
 		List<Long> stones = Arrays.stream(input.strip().split(" ")).map((s) -> Long.parseLong(s))
 				.collect(Collectors.toList());
 
-		System.out.println("Stones: " + stones.size());
-
 		for (int blink = 0; blink < blinks; blink++) {
 			for (int index = stones.size() - 1; index >= 0; --index) {
 				if (stones.get(index) == 0) {
@@ -65,56 +62,15 @@ public class Solution
 				}
 				stones.set(index, stones.get(index) * 2024L);
 			}
-			System.out.println("Stones: " + stones.size());
+
+			System.out.println(String.format("%10s @ %2d / %2d", input, blink, blinks));
+
+			// System.out.println("Stones: " + stones.size());
 		}
 
 		return stones.size();
 	}
 
-	public static class StoneRunnable implements RunnableFuture<Long> {
-
-		private final String startStone;
-
-		/**
-		 * Computed in {@link run()}.
-		 */
-		private long result = -1;
-
-		public StoneRunnable(String startStone) {
-			this.startStone = startStone;
-		}
-
-		@Override
-		public boolean cancel(boolean mayInterruptIfRunning) {
-			return false;
-		}
-
-		@Override
-		public boolean isCancelled() {
-			return false;
-		}
-
-		@Override
-		public boolean isDone() {
-			return result != -1 /* default value */;
-		}
-
-		@Override
-		public Long get() throws InterruptedException, ExecutionException {
-			return result;
-		}
-
-		@Override
-		public Long get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-			return result;
-		}
-
-		@Override
-		public void run() {
-			result = (long) solvePart1(startStone, BLINKS_PART2);
-		}
-	}
-	
 	/**
 	 * Solve the second part.
 	 * 
@@ -122,24 +78,40 @@ public class Solution
 	 * @return The solution to the second part.
 	 */
 	protected static Object solvePart2(String input) {
-		List<String> stones = Arrays.asList(input.strip().split(" "));
-		ExecutorService threadPool = Executors.newFixedThreadPool(stones.size());
+		String[] stones = input.strip().split(" ");
+
+		System.out.println("the stones for Part 2: " + stones);
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(stones.length);
 
 		long totalSize = 0;
 
-		List<StoneRunnable> threads = new ArrayList<StoneRunnable>(stones.size());
+		List<Future<Long>> threads = new ArrayList<Future<Long>>(stones.length);
 
 		for (String stone : stones) {
-			StoneRunnable thread = new StoneRunnable(stone);
-			threads.add(thread);
-			threadPool.submit(thread);
+			threads.add(threadPool.submit(new Callable<Long>() {
+				@Override
+				public Long call() throws Exception {
+					System.out.println("I will execute the stones for " + stone);
+					Long result = (Long) solvePart1(stone, BLINKS_PART2);
+					System.out.println("Done executing stones for " + stone);
+					return result;
+				}
+			}));
+		}
+
+		for (Future<Long> completed : threads) {
+			try {
+				totalSize += completed.get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
 
 		threadPool.shutdown();
 
-		for (StoneRunnable completed : threads) {
-			totalSize += completed.result;
-		}
 		return totalSize;
 	}
 }

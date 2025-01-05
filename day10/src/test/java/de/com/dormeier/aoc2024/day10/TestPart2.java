21ba660b6cfde1b30ad31b10a6e820cf5a98040b
Philipp <30900810+DerReparator@users.noreply.github.com>
2025-01-05 12:54:17 +0100

    setup tests for Day 10, Part 2

--- day10/src/test/java/de/com/dormeier/aoc2024/day10/TestPart2.java
+++ day10/src/test/java/de/com/dormeier/aoc2024/day10/TestPart2.java
@@ -27,4 +27,51 @@ public class TestPart2 {
 
 		assertEquals(expectedPart2_1, result.toString());
 	}
+
+	@Test
+	public void ShouldIdentifyTrailheadWithRating13Correctly() {
+		String input = """
+				..90..9
+				...1.98
+				...2..7
+				6543456
+				765.987
+				876....
+				987....
+				""";
+		Object result = Solution.solvePart2(input);
+
+		assertEquals(13, result);
+	}
+
+	@Test
+	public void ShouldIdentifyTrailheadWithRating3Correctly() {
+		String input = """
+				.....0.
+				..4321.
+				..5..2.
+				..6543.
+				..7..4.
+				..8765.
+				..9....
+				""";
+		Object result = Solution.solvePart2(input);
+
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void ShouldIdentifyTrailheadWithRating227Correctly() {
+		String input = """
+				012345
+				123456
+				234567
+				345678
+				4.6789
+				56789.
+				""";
+		Object result = Solution.solvePart2(input);
+
+		assertEquals(227, result);
+	}
 }

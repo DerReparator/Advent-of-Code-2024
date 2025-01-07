12fe7431780110dad867f8c069644077854ccb7f
Philipp <30900810+DerReparator@users.noreply.github.com>
2025-01-07 20:34:13 +0100

    solved Day 12, Part 1

--- day12/python/day12_1.py
+++ day12/python/day12_1.py
@@ -2,36 +2,59 @@
 
 from collections import defaultdict
 from dataclasses import dataclass
+from typing import Tuple
 
-@dataclass
+@dataclass(eq=True)
 class Region:
-    region_perim = defaultdict(int)
-    region_area = defaultdict(int)
+    perimeter: int = 0
+    area: int = 0
+
+    def calculate_cost(self) -> int:
+        return self.perimeter * self.area
 
 class Solution:
     def __init__(self):
         self.dirY = [-1, 0, 1, 0]
         self.dirX = [0, 1, 0, -1]
-        self.handledSpots = {}
+        self.handledSpots = set()
 
     def solvePart1(self, input: str) -> int:
-        map = [[c for c in line.strip()] for line in input.strip().splitlines()]
+        self.map = [[c for c in line.strip()] for line in input.splitlines()]
 
-        for y in range(len(map)):
-            for x in range(len(map[x])):
+        total_price = 0
+        for y in range(len(self.map)):
+            for x in range(len(self.map[y])):
                 if (x,y) in self.handledSpots:
                     continue
                 else:
-                    complete_region: Region = discover_new_region((x,y))
-                region_area[map[y][x]] += 1
-                for dir in range(len(self.dirX)):
-                    if y + self.dirY[dir] < 0 or y + self.dirY[dir] > len(map)\
-                    or x + self.dirX[dir] < 0 or x + self.dirX[dir] > len(map[x])\
-                    or map[y + self.dirY[dir]][x + self.dirX[dir]] != map[y][x]:
-                        region_perim[map[y][x]] += 1
-
-        total_price = 0
-        for plant, area in region_area:
-            total_price += area * region_perim
+                    complete_region: Region = self.discover_new_region((x,y))
+                    total_price += complete_region.calculate_cost()
         return total_price
-                
+    
+    def discover_new_region(self, startPoint: Tuple[int, int]):
+        region = Region()
+        self.visit_point(startPoint, region)
+        return region
+
+    def visit_point(self, point: Tuple[int, int], region: Region):
+        if point in self.handledSpots:
+            return
+        self.handledSpots.add(point)
+        region.area += 1
+        x,y = point
+        for dir in range(len(self.dirX)):
+            next_point = (x + self.dirX[dir], y + self.dirY[dir])
+            if self.is_out_of_bounds(next_point)\
+            or self.map[y + self.dirY[dir]][x + self.dirX[dir]] != self.map[y][x]:
+                region.perimeter += 1
+            else:
+                self.visit_point(next_point, region)
+            
+    def is_out_of_bounds(self, point) -> bool:
+        x,y = point
+        return y < 0 or y >= len(self.map) or x < 0 or x >= len(self.map[y])
+
+if __name__=='__main__':
+    solver = Solution()
+    with open("input", 'r') as input_part1:
+        print(f"Solution Part 1: {solver.solvePart1(input_part1.read())}")
